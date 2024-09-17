a3c636ad75e0bc1e37292593e36d4b0ca2328693
Joseph Nguyen <yoseple23@gmail.com>
2024-09-17 18:20:42 -0400

    base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/reefqr/', // Add this line to specify the base URL for GitHub Pages
   plugins: [react()],
   css: {
-    postcss: './postcss.config.js',
+    postcss: './postcss.config.js', // Keep your PostCSS config as is
   },
 });
